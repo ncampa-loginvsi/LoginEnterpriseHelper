8afd6232dd34e3a8ae3feebba8b0b9f3753d4adf
Nick <99031249+ncampa-loginvsi@users.noreply.github.com>
2022-12-05 08:49:08 -0500

    Update AutoLogon.ps1

--- AutoLogon.ps1
+++ AutoLogon.ps1
@@ -1,6 +1,12 @@
 # This script downloads chocolatey package manager, in order to install Autologon. 
 # Upon reboot, the script will login as $DefaultUsername, $DefaultPassword, using the $DefaultDomain
 # The number of automatic logins is dictated by $AutoLogonCount i.e. $AutoLogonCount = 1 logs in once
+Param(
+  $DefaultUsername, 
+  $DefaultPassword, 
+  $DefaultDomain, 
+  $AutoLogonCount
+)
 
 # Download chocolatey package manager
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
@@ -10,10 +16,6 @@ choco install autologon -y
 
 # Set Registry credentials for Autologon
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$DefaultUsername = ""
-$DefaultDomain = ""
-$DefaultPassword = ""
-$AutoLogonCount = ""
 
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
 Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
